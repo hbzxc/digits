45ced8c36afe97dc8d3ea87e682890f2d8c63434
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-11-05 22:20:21 -1000

    updated edit stuff
    changed to edit contacts

--- app/imports/ui/pages/EditContact.jsx
+++ app/imports/ui/pages/EditContact.jsx
@@ -31,7 +31,7 @@ class EditContacts extends React.Component {
     return (
       <Grid container centered>
         <Grid.Column>
-          <Header as="h2" textAlign="center">Edit Stuff</Header>
+          <Header as="h2" textAlign="center" inverted>Edit Contacts</Header>
           <AutoForm schema={bridge} onSubmit={data => this.submit(data)} model={this.props.doc}>
             <Segment>
               <TextField name='firstName'/>
