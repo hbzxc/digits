532d702be11d9d8875897f9e064f78b042b60e84
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-11-07 23:20:59 -1000

    done
    finished in 22 minutes

--- app/imports/api/note/Notes.js
+++ app/imports/api/note/Notes.js
@@ -5,31 +5,29 @@ import { Tracker } from 'meteor/tracker';
 /**
  * The Notes. It encapsulates state and variable values for stuff.
  */
-class ContactsCollection {
+class NotesCollection {
   constructor() {
     // The name of this collection.
-    this.name = 'Contacts';
+    this.name = 'NotesCollection';
     // Define the Mongo collection.
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
-      firstName: String,
-      lastName: String,
-      address: String,
-      image: String,
-      description: String,
+      note: String,
+      contactId: String,
       owner: String,
+      createdAt: Date,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
     // Define names for publications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
-    this.adminPublicationName = `${this.name}.publication.admin`;
+    // this.adminPublicationName = `${this.name}.publication.admin`;
   }
 }
 
 /**
  * The singleton instance of the Notes.
- * @type {Contacts}
+ * @type {Notes}
  */
-export const Contacts = new ContactsCollection();
+export const Notes = new NotesCollection();
